aa6fb4651b0605c4450dafd82001dc3a3c5aed03
MwambiaBrian <brianmwambia4@gmail.com>
2024-10-25 15:04:55 +0300

    database set up

--- apps/web/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from 'next/image'
-import { add } from '@parkingX/sample-lib'
-export default function Home() {
-  return (
-    <div>
-      <h1>ParkingX {add(3, 4)}</h1>
-    </div>
-  )
-}
